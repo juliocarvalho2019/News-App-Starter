67ad6db9da418f225f535b891394e2dbd41211e8
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-30 15:34:49 -0300

    Feat: Criado função searchNews para busca de noticis.

--- News-App-Starter/app/src/main/java/br/com/carvalho/newsappstarter/model/data/NewsDataSource.kt
+++ News-App-Starter/app/src/main/java/br/com/carvalho/newsappstarter/model/data/NewsDataSource.kt
@@ -2,6 +2,7 @@ package br.com.carvalho.newsappstarter.model.data
 
 import br.com.carvalho.newsappstarter.network.RetrofitInstance
 import br.com.carvalho.newsappstarter.presenter.news.NewsHome
+import br.com.carvalho.newsappstarter.presenter.search.SearchHome
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -11,6 +12,21 @@ class NewsDataSource {
     fun getBreakingNews(callback: NewsHome.Presenter) {
         GlobalScope.launch(Dispatchers.Main) {
             val response = RetrofitInstance.api.getBreakingNews("br")
+            if (response.isSuccessful) {
+                response.body()?.let { newsResponse ->
+                    callback.onSuccess(newsResponse)
+                }
+                callback.onComplete()
+            } else {
+                callback.onError(response.message())
+                callback.onComplete()
+            }
+        }
+    }
+
+    fun searchNews(term: String, callback: SearchHome.Presenter) {
+        GlobalScope.launch(Dispatchers.Main) {
+            val response = RetrofitInstance.api.searchNews(term)
             if (response.isSuccessful) {
                 response.body()?.let { newsResponse ->
                     callback.onSuccess(newsResponse)
