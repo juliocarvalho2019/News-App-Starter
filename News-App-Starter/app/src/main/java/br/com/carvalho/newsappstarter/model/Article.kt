938720dadc9ce54d74eee52f3e1b455e19521ad4
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-31 15:19:46 -0300

    Feat:setup para Romm01.

--- News-App-Starter/app/src/main/java/br/com/carvalho/newsappstarter/model/Article.kt
+++ News-App-Starter/app/src/main/java/br/com/carvalho/newsappstarter/model/Article.kt
@@ -1,8 +1,12 @@
 package br.com.carvalho.newsappstarter.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.io.Serializable
-
+@Entity(tableName = "articles")
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
     val author: String?,
     val content: String?,
     val description: String?,
