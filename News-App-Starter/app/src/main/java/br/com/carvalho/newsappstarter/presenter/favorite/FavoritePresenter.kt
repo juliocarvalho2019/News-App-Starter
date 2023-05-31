47dcbbaceb4341563be12b0ec6568de36724f424
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-31 16:57:17 -0300

    Feat: Deletando noticias favoritas.

--- News-App-Starter/app/src/main/java/br/com/carvalho/newsappstarter/presenter/favorite/FavoritePresenter.kt
+++ News-App-Starter/app/src/main/java/br/com/carvalho/newsappstarter/presenter/favorite/FavoritePresenter.kt
@@ -6,13 +6,19 @@ import br.com.carvalho.newsappstarter.presenter.ViewHome
 
 class FavoritePresenter(
     val view: ViewHome.Favorite,
-    private val dataSource: NewsDataSource) : FavoriteHome.Presenter {
+    private val dataSource: NewsDataSource
+) : FavoriteHome.Presenter {
 
-    fun getAll(){
+    fun getAll() {
         this.dataSource.getAllArticle(this)
     }
+
     fun saveArticle(article: Article) {
         this.dataSource.saveArticle(article)
+    }
+
+    fun deleteArticle(article: Article) {
+        this.dataSource.deleArticle(article)
     }
 
     override fun onSuccess(articles: List<Article>) {
--- News-App-Starter/app/src/main/java/br/com/carvalho/newsappstarter/ui/FavoriteActivity.kt
+++ News-App-Starter/app/src/main/java/br/com/carvalho/newsappstarter/ui/FavoriteActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import br.com.carvalho.newsappstarter.R
 import br.com.carvalho.newsappstarter.adapter.MainAdapter
 import br.com.carvalho.newsappstarter.databinding.ActivityFavoriteBinding
@@ -16,8 +18,9 @@ import br.com.carvalho.newsappstarter.model.data.NewsDataSource
 import br.com.carvalho.newsappstarter.presenter.ViewHome
 import br.com.carvalho.newsappstarter.presenter.favorite.FavoritePresenter
 import br.com.carvalho.newsappstarter.presenter.search.SearchPresenter
+import com.google.android.material.snackbar.Snackbar
 
-class FavoriteActivity : AppCompatActivity(), ViewHome.Favorite  {
+class FavoriteActivity : AppCompatActivity(), ViewHome.Favorite {
 
     private val mainAdapter by lazy {
         MainAdapter()
@@ -25,7 +28,7 @@ class FavoriteActivity : AppCompatActivity(), ViewHome.Favorite  {
 
     private lateinit var binding: ActivityFavoriteBinding
     private lateinit var presenter: FavoritePresenter
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFavoriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -35,6 +38,41 @@ class FavoriteActivity : AppCompatActivity(), ViewHome.Favorite  {
         presenter.getAll()
         configRecycle()
         clickAdapter()
+
+        val itemTouchPerCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val article = mainAdapter.differ.currentList[position]
+                presenter.deleteArticle(article)
+                Snackbar.make(
+                    viewHolder.itemView, R.string.article_delete_successful,
+                    Snackbar.LENGTH_SHORT
+                ).apply {
+                    setAction(getString(R.string.undo)) {
+                        presenter.saveArticle(article)
+                        mainAdapter.notifyDataSetChanged()
+                    }
+                    show()
+                }
+            }
+
+        }
+
+        ItemTouchHelper(itemTouchPerCallback).apply {
+            attachToRecyclerView(binding.rvFavorite)
+        }
+        presenter.getAll()
     }
 
     private fun configRecycle() {
@@ -43,7 +81,8 @@ class FavoriteActivity : AppCompatActivity(), ViewHome.Favorite  {
             layoutManager = LinearLayoutManager(this@FavoriteActivity)
             addItemDecoration(
                 DividerItemDecoration(
-                    this@FavoriteActivity, androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
+                    this@FavoriteActivity,
+                    androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
                 )
             )
         }
