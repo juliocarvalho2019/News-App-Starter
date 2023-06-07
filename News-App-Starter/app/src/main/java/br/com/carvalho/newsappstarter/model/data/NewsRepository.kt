5bbff72a0ebba948e7e23a8e198f74c93030c4d2
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-06-06 21:36:51 -0300

    Feat: Finalizado projeto.

--- News-App-Starter/app/src/main/java/br/com/carvalho/newsappstarter/model/data/NewsRepository.kt
+++ News-App-Starter/app/src/main/java/br/com/carvalho/newsappstarter/model/data/NewsRepository.kt
@@ -5,7 +5,7 @@ import br.com.carvalho.newsappstarter.model.db.ArticleDatabase
 
 class NewsRepository(private val db: ArticleDatabase) {
 
-    suspend fun updateInsert(article:Article) = db.getArticleDao().updateInsert(article)
+    suspend fun updateInsert(article: Article) = db.getArticleDao().updateInsert(article)
 
     fun getAll(): List<Article> = db.getArticleDao().getAll()
 
